31726db97600d7239da5794cd865ccf333ce0a37
LaetitiaCarquet <laetitiacarquet@gmail.com>
2017-04-27 14:47:40 +0000

    set up static folder fix #4

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.sendFile('./public/index.html')
 });
 
 app.listen(port, ()=> {
