49a02f70c86bdd71b75059f1a6498315cb1db7e9
LaetitiaCarquet <laetitiacarquet@gmail.com>
2017-04-27 15:10:04 +0000

    set the routes for queries related to #8

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.get('/', (req, res) => {
     res.sendFile('./public/index.html')
 });
 
+app.get('/api/v1/news', (req, res)=> {
+    let start = req.query.start || "by default"
+    let end = req.query.end || "by default"
+    let size = req.query.size || "by default"
+    let page = req.query.page || "by default"
+    res.send(`welcome to my API !
+    start: ${start}
+    end: ${end}
+    size: ${size}
+    page: ${page}`)
+    
+})
+
 app.listen(port, ()=> {
     console.log(`server is on port ${port}`)
 })
