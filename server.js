8ec22706535dfec45ecced8f8729416c3eb3fb85
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-05-07 18:51:21 +0000

    MySQL Concept

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const exec = require('child_process').exec;
 const app = express();
 const dummyData = require('./public/demo.json');
 const config = require('./config');
+const mysql      = require('mysql');
+const dbConnection = mysql.createConnection({
+    host     : config.database.host,
+    user     : config.database.user,
+    password : config.database.password,
+    database : config.database.database
+});
+
 
 const port = process.env.PORT || 3000
 
@@ -33,6 +41,23 @@ app.get('/api/v1/news', (req, res)=> {
     - end: ${end}
     - size: ${size}
     - page: ${page}`);
+    
+    // MySQL Concept - TEST
+    dbConnection.connect();
+
+    var baseQuery = `SELECT articulo.id_noticia, articulo.fecha, articulo.titulo, articulo.texto_noticia, articulo.url, GROUP_CONCAT(CONCAT(tags.id_tag, ':{', tags.visitas, '}'))
+FROM articulo
+INNER JOIN tags
+ON articulo.id_noticia=tags.id_noticia
+GROUP BY id_noticia;`;
+
+    dbConnection.query(baseQuery, function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results);
+    });
+
+    dbConnection.end();
+    
 })
 
 app.listen(port, ()=> {
@@ -43,7 +68,7 @@ app.listen(port, ()=> {
 // Cron Tasks
 var rRocks = new Scheduled({
     id: "rRocks",
-    pattern: "* */4 * * * *",
+    pattern: "00 10 * * * *", // 10:00 Every day.
     task: function() {
         exec(`Rscript Recopile_data/getData_clean.R ${config.nytToken}`, function(error, stdout, stderr) {
             console.log(`---- Proceso hijo (Recopile_data/getData_clean.R) terminado! -----`);
