ed19548a0d083935395d220de75ca9717f4818ee
Rocío Gómez Sánchez <rociogosa@hotmail.com>
2017-05-11 13:54:00 +0200

    Add files via upload

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,23 +1,27 @@
 var oReq = new XMLHttpRequest()
 oReq.addEventListener("load", reqListener)
-  oReq.open("GET", "https://raw.githubusercontent.com/rladies/news/master/public/demo.json")
+  oReq.open("GET", "http://news.osweekends.com/api/v1/news")
 oReq.send()
 
-// var title = document.getElementById('followers')
-// var sentimiento = document.getElementById('followers')
-// var visitasLabel = document.getElementById('followers')
-
-// for (var i = 0; i < Things.length; i++) {
-// 	Things[i]
-// }
-
 function sentimentBars(sentiment){
-  if (sentiment <0.2 ) return "rgba(255, 99, 132, 0.2)";
-  if (sentiment <0.8) return 'rgba(255, 159, 64, 0.2)';
+  if (sentiment < 0 ) return "rgba(255, 99, 132, 0.5)";
+  if (sentiment > 0) return 'rgba(75, 192, 192, 0.5)';
 
-  return 'rgba(75, 192, 192, 0.2)';
+  return 'rgba(255, 159, 64, 0.5)';
 }
 
+
+function stimateVisits (rawVisits) {
+  var total = 0
+  rawVisits.forEach(function(row){
+    console.log(row)
+    if(row.valores && row.valores.length === 60){
+      total += parseInt(row.valores[59])
+    }
+    
+  })
+  return total;
+}
 
 function reqListener () {
   var data = JSON.parse(this.responseText);
@@ -28,10 +32,8 @@ function reqListener () {
   var numVis = [];
   for (var i = 0; i < data.length; i++) {
     titles.push(data[i].titulo);
-    backgroundColors.push(sentimentBars(data[i].sentimiento));
-    borderColors.push('rgba(255,99,132,1)');
-    numVis.push(data[i].visitas);
-
+    backgroundColors.push(sentimentBars(data[i].sentiment_value));
+    numVis.push(stimateVisits(data[i].visitas));
   }
   
   var ctx = document.getElementById("myChart");
